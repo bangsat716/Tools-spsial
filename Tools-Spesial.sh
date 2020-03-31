cc1c1426a516fcbc5d972b1f5c2604461995695b
bangsat716 <61156264+bangsat716@users.noreply.github.com>
2020-04-01 02:35:33 +0700

    Add files via upload

---
+++ Tools-Spesial.sh
@@ -0,0 +1,209 @@
+#!/usr/bin/Shell
+
+# Author : M_aref
+# Youtub : The-X-Cyber
+# Recode Tidak Akan Membuat Anda Menjadi Pencipta Kode :3
+
+k="\033[33;1m"
+m="\033[31;1m"
+pu="\033[39;1m"
+p="\033[36;1m"
+b="\033[34;1m"
+h="\033[32;1m"
+JAM=`date +%H`
+MENIT=`date +%M`
+DETIK=`date +%S`
+TANGGAL=`date +%d`
+BULAN=`date +%m`
+TAHUN=`date +%Y`
+mp="\033[41;1;39m"
+o="\033[0m"
+clear
+python nama.py
+sleep 2
+termux-open-url 'https://m.youtube.com/channel/UCVaKVhhcdngVG1aGjzDLFmg'
+sleep 2
+clear
+echo
+echo "         ${m}[ ${pu}TIME ZONE INDONESIA ${m}]"
+echo "${b}   ╔════════╗${b} ╔═══════🕜═╗${b} ╔═══════🕛═╗"
+echo "${b}   ║${k}JAM:"${m}[${pu}$JAM${m}]${b}║~${b}║${k}MENIT:"${m}[${pu}$MENIT${m}]${b}║${b}~║${k}DETIK:${m}["${pu}$DETIK${m}]${b}║
+echo "${b}   ╚════════╝${b} ╚══════════╝${b} ╚══════════╝"
+echo "               ${m}[ ${pu}KALENDER ${m}]"
+echo "${b}╔════════════╗${b} ╔══════════╗${b} ╔════════════╗"
+echo "${b}║${k}TANGGAL:"${m}[${pu}$TANGGAL${m}]${b}║${b}~║${k}BULAN:"${m}[${pu}$BULAN${m}]${b}║${b}~║${k}TAHUN:${m}["${pu}$TAHUN${m}]${b}║
+echo "${b}╚════════════╝${b} ╚══════════╝${b} ╚════════════╝"
+echo "${k}<════════════════════════════════════════>"
+echo
+sleep 2
+python2 R.py
+sleep 2
+clear
+python2 A.py
+sleep 2
+clear
+bash logo.sh
+limit=100
+sleep 2
+python2 M.py
+echo "\033[1;92m-----------------------------------------------"
+echo "\033[031m▇▇▇▇▇▇▇ \033[1;91m( \033[1;37mAuthor \033[1;91m) \033[1;34m: \033[1;33mM_aref"
+echo "\033[037m▇▇▇▇▇▇▇   \033[1;92m------   \033[1;34m:                  \033[1;33mversi \033[1;92m9.9"
+echo " The X  \033[1;91m( \033[1;37mYoutub \033[1;91m) \033[1;34m: \033[1;33mThe-X-Cyber"
+echo "\033[1;92m-----------------------------------------------"
+sleep 2
+echo "\033[1;91m[\033[1;37m★\033[1;91m]\033[1;37m=========================================\033[1;91m[\033[1;37m★\033[1;91m]"
+echo "\033[1;92mON \033[1;91m[\033[1;37m01\033[1;91m] \033[1;92m: \033[1;37mHack Facebook"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;92mON \033[1;91m[\033[1;37m02\033[1;91m] \033[1;92m: \033[1;37mHack Instagram"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;92mON \033[1;91m[\033[1;37m03\033[1;91m] \033[1;92m: \033[1;37mSpam Whatsapp"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;92mON \033[1;91m[\033[1;37m04\033[1;91m] \033[1;92m: \033[1;37mA-RAT"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;92mON \033[1;91m[\033[1;37m05\033[1;91m] \033[1;92m: \033[1;37mBelajar Coding"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;92mON \033[1;91m[\033[1;37m06\033[1;91m] \033[1;92m: \033[1;37mReport Facebook"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;92mON \033[1;91m[\033[1;37m07\033[1;91m] \033[1;92m: \033[1;37mSpam Call"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;92mON \033[1;91m[\033[1;37m08\033[1;91m] \033[1;92m: \033[1;37mPHISING"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;92mON \033[1;91m[\033[1;37m09\033[1;91m] \033[1;92m: \033[1;37mProfil Guard Facebook"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;92mON \033[1;91m[\033[1;37m10\033[1;91m] \033[1;92m: \033[1;37mWEBDAV"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;33m!! \033[1;91m[\033[1;37m11\033[1;91m] \033[1;92m: \033[1;37mInstall bahan"
+echo "   \033[1;37m---- \033[1;92m:"
+echo "\033[1;34m?? \033[1;91m[\033[1;37m00\033[1;91m] \033[1;92m: \033[1;37mExit"
+echo "\033[1;91m[\033[1;37m★\033[1;91m]\033[1;37m=======================================\033[1;91m[\033[1;37m★\033[1;91m]\033[1;37m"
+read -p "   Thex »--> : " pil
+
+if [ $pil = 1 ] || [ $pil = 01 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+git clone https://github.com/bangsat716/Dark-fb
+cd Dark-fb
+python2 Darkfb.py
+fi
+
+if [ $pil = 2 ] || [ $pil = 02 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+git clone https://github.com/bangsat716/Hack-IG
+cd Hack-IG
+bash ig.sh
+fi
+
+if [ $pil = 3 ] || [ $pil = 03 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+git clone https://github.com/bangsat716/Spam-Whatsapp
+cd Spam-Whatsapp
+python2 Spam-Whatsapp.py
+fi
+
+if [ $pil = 4 ] || [ $pil = 04 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+git clone https://github.com/bangsat716/A-RAT
+cd A-RAT
+python2 A-Rat.py
+fi
+
+if [ $pil = 5 ] || [ $pil = 05 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+git clone https://github.com/bangsat716/belajar-Coding
+cd belajar-Coding
+sh belajar-coding.sh
+fi
+
+if [ $pil = 6 ] || [ $pil = 06 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 7 ] || [ $pil = 07 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+git clone https://github.com/bangsat716/Spam-telpon
+cd Spam-telpon
+bash call.sh
+fi
+
+if [ $pil = 8 ] || [ $pil = 08 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+git clone https://github.com/evait-security/weeman
+cd weeman
+python2 weeman.py
+fi
+
+if [ $pil = 9 ] || [ $pil = 09 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+git clone https://github.com/bangsat716/propil-fb
+cd propil-fb
+php fb.php
+fi
+
+if [ $pil = 10 ] || [ $pil = 10 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+git clone https://github.com/bangsat716/deface
+cd deface
+sh Webdav.sh
+fi
+
+if [ $pil = 11 ] || [ $pil = 11 ]
+then
+clear
+echo "  \033[1;92mTunggu sebentar\033[1;37m"
+python2 A.py
+sleep 3
+sh install.sh
+sh Tools-Spesial.sh
+fi
+
+if [ $pil = 0 ] || [ $pil = 00 ]
+then
+clear
+echo "  \033[1;92mBy Goblokk \033[1;91m!!\033[1;37m"
+sleep 4
+exit
+fi
