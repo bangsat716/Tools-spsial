7bd036050bfaa44e00687f3be2094c2e955f63db
bangsat716 <61156264+bangsat716@users.noreply.github.com>
2020-04-04 02:43:59 +0700

    Add files via upload

---
+++ Tanggal.py
@@ -0,0 +1,23 @@
+M = '\033[1;91m'
+H = '\033[1;92m'
+K = '\033[1;33m'
+B = '\033[1;34m'
+P = '\033[1;37m'
+BM = '\033[036m'
+import os
+import random
+import sys
+import time
+def TheX (s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(1./12)
+os.system('clear')
+TheX(M + 'TOOLS DI PERBARUI jum,at 03 April wib 19:45')
+TheX(H + 'Author      : M_aref')
+TheX(K + 'Youtube     : The-X-Cyber')
+TheX(B + 'Whatsapp    : 083177336282')
+TheX(P + 'Gmail       : arefm425@gmail.com')
+TheX(BM + 'Instagram   : @ardinaa521')
+TheX(M + 'Facebook    : M-aref')
