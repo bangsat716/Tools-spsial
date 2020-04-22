718312d2ba4b912610d125c3eca2b73dfc7ea0ff
bangsat716 <61156264+bangsat716@users.noreply.github.com>
2020-04-23 01:06:53 +0700

    Update Tanggal.py

--- Tanggal.py
+++ Tanggal.py
@@ -14,7 +14,7 @@ def TheX (s):
                 sys.stdout.flush()
                 time.sleep(1./12)
 os.system('clear')
-TheX(M + 'TOOLS DI PERBARUI Sabtu 04 April wib 02:45')
+TheX(M + 'TOOLS DI UPDATE kamis 23 April wib 01:10')
 TheX(H + 'Author      : M_aref')
 TheX(K + 'Youtube     : The-X-Cyber')
 TheX(B + 'Whatsapp    : 083177336282')
